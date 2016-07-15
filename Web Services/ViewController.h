13f4b4a90c2c25c3acfa36e1c080ecce14622238
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2016-07-15 18:40:00 -0500

    Initial Commit

---
+++ Web Services/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Web Services
+//
+//  Created by Walter Gonzalez Domenzain on 15/07/16.
+//  Copyright © 2016 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
